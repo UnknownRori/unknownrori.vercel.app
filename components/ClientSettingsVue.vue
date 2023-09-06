cf570580df182eb5804db6226bacc5d5c0da4209
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-09-07 06:06:34 +0700

    📦 Extract popout into seperate component

--- components/ClientSettingsVue.vue
+++ components/ClientSettingsVue.vue
@@ -11,76 +11,70 @@ const isOpen = ref(false);
     <SettingsIcon class="w-8 h-8 m-0 p-0" :fontControlled="false" filled />
   </button>
 
-  <Teleport to="body">
-    <div
-      v-if="isOpen"
-      aria-label="Modal Wrapper"
-      class="fixed flex justify-center items-center w-screen h-screen text-white bg-slate-950 bg-opacity-40"
+  <PopoutVue :is-open="isOpen">
+    <CardVue
+      aria-label="Modal"
+      class="relative min-w-[42vw] max-w-[42vw] flex flex-col gap-2 p-4"
     >
-      <CardVue
-        aria-label="Modal"
-        class="relative min-w-[42vw] max-w-[42vw] flex flex-col gap-2 p-4"
-      >
-        <header class="relative">
-          <h1 class="text-center font-bold text-2xl drop-shadow-lg antialiased">
-            Preferences
-          </h1>
+      <header class="relative">
+        <h1 class="text-center font-bold text-2xl drop-shadow-lg antialiased">
+          Preferences
+        </h1>
 
-          <button @click="isOpen = false">
-            <CloseIcon
-              class="absolute h-8 w-8 right-0 top-0 text-cod-gray-800 hover:text-white duration-300"
-              :fontControlled="false"
-              filled
-            />
-          </button>
-        </header>
-        <main>
-          <form action="" class="flex flex-col gap-4">
-            <div class="flex flex-col gap-2 w-full">
-              <label for="display-type" class="flex gap-1">
-                Display type
-                <InfoIcon
-                  class="h-4 w-4 text-white"
-                  :fontControlled="false"
-                  filled
-                />
-              </label>
-              <select
-                id="display-type"
-                name="display-type"
-                class="bg-cod-gray-800 bg-opacity-80 rounded p-2 shadow"
-              >
-                <option value="Web">Web</option>
-                <option value="Terminal" disabled>Terminal</option>
-              </select>
-            </div>
+        <button @click="isOpen = false">
+          <CloseIcon
+            class="absolute h-8 w-8 right-0 top-0 text-cod-gray-800 hover:text-white duration-300"
+            :fontControlled="false"
+            filled
+          />
+        </button>
+      </header>
+      <main>
+        <form action="" class="flex flex-col gap-4">
+          <div class="flex flex-col gap-2 w-full">
+            <label for="display-type" class="flex gap-1">
+              Display type
+              <InfoIcon
+                class="h-4 w-4 text-white"
+                :fontControlled="false"
+                filled
+              />
+            </label>
+            <select
+              id="display-type"
+              name="display-type"
+              class="bg-cod-gray-800 bg-opacity-80 rounded p-2 shadow"
+            >
+              <option value="Web">Web</option>
+              <option value="Terminal" disabled>Terminal</option>
+            </select>
+          </div>
 
-            <div class="flex flex-col gap-2 w-full">
-              <label for="colorscheme" class="flex gap-1">
-                Colorscheme
-                <InfoIcon
-                  class="h-4 w-4 text-white"
-                  :fontControlled="false"
-                  filled
-                />
-              </label>
-              <select
-                id="colorscheme"
-                name="colorscheme"
-                class="bg-cod-gray-800 bg-opacity-80 rounded p-2 shadow"
-              >
-                <option value="dark-default">Dark Default</option>
-                <option value="light-default" disabled>Light Default</option>
-                <option value="monokai" disabled>Monokai</option>
-              </select>
-            </div>
+          <div class="flex flex-col gap-2 w-full">
+            <label for="colorscheme" class="flex gap-1">
+              Colorscheme
+              <InfoIcon
+                class="h-4 w-4 text-white"
+                :fontControlled="false"
+                filled
+              />
+            </label>
+            <select
+              id="colorscheme"
+              name="colorscheme"
+              class="bg-cod-gray-800 bg-opacity-80 rounded p-2 shadow"
+            >
+              <option value="dark-default">Dark Default</option>
+              <option value="light-default" disabled>Light Default</option>
+              <option value="monokai" disabled>Monokai</option>
+            </select>
+          </div>
 
-            <div class="flex justify-end">
-              <button class="bg-green-600 rounded py-2 px-4">Save</button>
-            </div>
-          </form>
-        </main>
-      </CardVue>
-    </div>
-  </Teleport>
+          <div class="flex justify-end">
+            <button class="bg-green-600 rounded py-2 px-4">Save</button>
+          </div>
+        </form>
+      </main>
+    </CardVue>
+  </PopoutVue>
 </template>
---
+++ components/PopoutVue.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps<{
+  isOpen: boolean;
+}>();
+</script>
+
+<template>
+  <Teleport to="body">
+    <div
+      v-if="isOpen"
+      aria-label="Modal Wrapper"
+      class="fixed flex justify-center items-center w-screen h-screen text-white bg-slate-950 bg-opacity-40"
+    >
+      <slot />
+    </div>
+  </Teleport>
+</template>
