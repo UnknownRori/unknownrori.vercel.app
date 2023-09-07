2088d7be826c3b0a3056d0635f936a8a4cfa59ef
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-09-07 12:47:53 +0700

    📦 Added spoiler

--- app.vue
+++ app.vue
@@ -1,5 +1,38 @@
+<script setup lang="ts">
+import InfoIcon from "~/assets/icon/Info.svg";
+
+// TODO : Make this only appear once in every visit
+const underConstructionWarning = ref(true);
+</script>
+
 <template>
   <NuxtLoadingIndicator />
+
+  <PopoutVue
+    :is-open="underConstructionWarning"
+    @click-outside="underConstructionWarning = false"
+  >
+    <CardVue class="flex flex-col gap-4 p-5 min-w-[30vw] max-w-[30vw]">
+      <header class="flex items-center gap-4">
+        <InfoIcon class="w-12 h-12" :font-controlled="false" filled />
+        <h1 class="text-2xl">Alpha Version</h1>
+      </header>
+      <main class="flex flex-col gap-4">
+        <p class="text-justify">
+          This site is in active development, the layout, responsiveness, mobile
+          support, color may changes throughout development cycles. feel free to
+          fill an issue if something is really bad happening
+        </p>
+        <button
+          class="bg-green-500 font-bold rounded p-2"
+          @click="underConstructionWarning = false"
+        >
+          Ok
+        </button>
+      </main>
+    </CardVue>
+  </PopoutVue>
+
   <NuxtLayout name="default-layout">
     <NavigationBarVue />
     <div class="h-8 overflow-hidden mx-10">
--- components/PopoutVue.vue
+++ components/PopoutVue.vue
@@ -2,16 +2,41 @@
 defineProps<{
   isOpen: boolean;
 }>();
+
+defineEmits<{
+  (e: "click-outside"): void;
+}>();
 </script>
 
 <template>
   <Teleport to="body">
-    <div
-      v-if="isOpen"
-      aria-label="Modal Wrapper"
-      class="fixed flex justify-center items-center w-screen h-screen text-white bg-slate-950 bg-opacity-40"
-    >
-      <slot />
-    </div>
+    <Transition>
+      <div
+        v-if="isOpen"
+        aria-label="Modal Wrapper"
+        class="fixed flex justify-center items-center w-screen h-screen text-white"
+      >
+        <div
+          @click="$emit('click-outside')"
+          class="absolute w-full h-full top-0 left-0 bg-slate-950 bg-opacity-40 z-0"
+        ></div>
+        <div class="z-10">
+          <slot />
+        </div>
+      </div>
+    </Transition>
   </Teleport>
 </template>
+
+<style scoped>
+/* we will explain what these classes do next! */
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
