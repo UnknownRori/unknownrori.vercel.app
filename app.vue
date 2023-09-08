804e3d7438eb68a56762a7f73d818fb4e476a207
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-09-08 13:47:55 +0700

    📦 Implement basic foundation of loading screen

--- app.vue
+++ app.vue
@@ -3,10 +3,81 @@ import InfoIcon from "~/assets/icon/Info.svg";
 
 // TODO : Make this only appear once in every visit
 const underConstructionWarning = ref(true);
+
+type LoadingContentType = {
+time: number, text: string
+}
+
+const route = useRoute();
+const startTime = new Date();
+const isDummyLoadingDone = ref(false);
+const currentPageFile = route.fullPath === "/" ? "/index.html" : `${route.fullPath}.html`;
+const loadingContentShown = ref([] as Array<LoadingContentType>);
+const BASE_DELAY = 10;
+const loadingContents = [
+  { time: 0, text: "Booting..." },
+  { time: 1, text: "Loading minimal resources..." },
+  { time: 4, text: currentPageFile },
+  { time: 12, text: "/assets/css/tailwind.css" },
+  { time: 14, text: "/assets/css/scroll.css" },
+  { time: 42, text: "/assets/css/nuxt.js" },
+  { time: 62, text: "Loading content..." },
+  { time: 82, text: "Content Loaded..." },
+  { time: 102, text: "Creating app..." },
+  { time: 199, text: "Mounting app..." },
+  { time: 214, text: "Starting app..." },
+  { time: 222, text: "Ready" },
+];
+
+const increment = 100 / loadingContents.length;
+const loadingLength = ref(increment);
+loadingContentShown.value.push(loadingContents[0]);
+
+onBeforeMount(async () => {
+  // for(let i = 9; i < loadingContents.length;i++) {
+  for(let i = 1; i < loadingContents.length;i++) {
+    loadingContentShown.value.push(loadingContents[i]);
+    loadingLength.value += increment;
+    await sleep(loadingContents[i].time);
+  }
+
+  isDummyLoadingDone.value = true;
+})
+
 </script>
 
+<style scoped>
+  .from-right-enter-active,
+.from-right-leave-active {
+  transition: all 200ms ease;
+}
+.from-right-enter-from,
+.from-right-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+</style>
+
 <template>
-  <NuxtLoadingIndicator />
+  <Transition name="fade">
+    <div v-if="!isDummyLoadingDone" id="loading" class="absolute inset-0 flex flex-col gap-2 justify-center items-center w-screen h-screen bg-black z-50">
+      <h2 class="text-lg text-white">Please wait warmly....</h2>
+      <div class="relative bg-cod-gray-950 rounded-md py-2 w-[24vw] lg:w-[42vw] md:w-[60vw] sm:w-[82vw]">
+        <span class="absolute top-0 left-0 rounded-md h-full bg-white" :style="`width: ${loadingLength}%`"></span>
+      </div>
+      <div class="relative min-h-[48vh] w-[24vw] lg:w-[42vw] md:w-[60vw] sm:w-[82vw]">
+        <div class="absolute inset-0 bg-gradient-to-b from-transparent to-black"></div>
+        <div class="flex flex-col-reverse justify-center items-start text-white overflow-hidden">
+          <TransitionGroup name="from-right">
+            <!-- <span v-for="(val, idx) in loadingContent" :key="idx">{{ val }}</span> -->
+            <span v-for="(val, idx) in loadingContentShown" :key="idx">[{{ "&nbsp;".repeat((5 - val.time.toString().length) >= 0 ? (5 - val.time.toString().length) : 0 ) + val.time }}] {{ val.text }}</span>
+          </TransitionGroup>
+        </div>
+      </div>
+    </div>
+  </Transition>
+
+
 
   <PopoutVue
     :is-open="underConstructionWarning"
@@ -40,6 +111,7 @@ const underConstructionWarning = ref(true);
     <div class="h-8 overflow-hidden mx-10">
       <div class="bg-[url(~/assets/icon/Seperator.svg)] bg-repeat-x py-4"></div>
     </div>
+    <NuxtLoadingIndicator />
 
     <main>
       <NuxtPage />
