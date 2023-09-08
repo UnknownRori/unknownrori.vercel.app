72bdba4e0a630e3a24eb33f13c1a4ff2b5ebcecb
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-09-09 03:49:14 +0700

    🛠️ Formatting

--- data/socialMediaLinks.ts
+++ data/socialMediaLinks.ts
@@ -19,23 +19,67 @@ import PackagistIcon from "~/assets/social-icon/packagist.webp";
 
 const socialMediaLinks = [
   { alt: "Github", href: "https://github.com/UnknownRori", src: GithubIcon },
-  { alt: "Stackoverflow", href: "https://stackoverflow.com/users/13991588/unknownrori", src: StackoverflowIcon },
-  { alt: "Stackexchange", href: "https://stackexchange.com/users/19154447/unknownrori", src: StackexchangeIcon },
-  { alt: "Wakatime", href: "https://wakatime.com/@UnknownRori", src: WakatimeIcon },
+  {
+    alt: "Stackoverflow",
+    href: "https://stackoverflow.com/users/13991588/unknownrori",
+    src: StackoverflowIcon,
+  },
+  {
+    alt: "Stackexchange",
+    href: "https://stackexchange.com/users/19154447/unknownrori",
+    src: StackexchangeIcon,
+  },
+  {
+    alt: "Wakatime",
+    href: "https://wakatime.com/@UnknownRori",
+    src: WakatimeIcon,
+  },
   { alt: "Itch.io", href: "https://unknownrori.itch.io/", src: ItchIOIcon },
-  { alt: "Instagram", href: "https://www.instagram.com/unknownrori/", src: InstagramIcon },
+  {
+    alt: "Instagram",
+    href: "https://www.instagram.com/unknownrori/",
+    src: InstagramIcon,
+  },
   { alt: "Twitter", href: "https://twitter.com/UnknownRori", src: TwitterIcon },
   { alt: "Facebook", href: "#", src: FacebookIcon },
   { alt: "Whatsapp", href: "#", src: WhatsappIcon },
-  { alt: "Discord", href: "https://discord.com/users/611902987744313345", src: DiscordIcon },
-  { alt: "Linkedin", href: "https://www.linkedin.com/in/unknownrori", src: LinkedinIcon },
-  { alt: "Proton Mail", href: "mailto:UnknownRori@proton.me", src: ProtonMailIcon },
+  {
+    alt: "Discord",
+    href: "https://discord.com/users/611902987744313345",
+    src: DiscordIcon,
+  },
+  {
+    alt: "Linkedin",
+    href: "https://www.linkedin.com/in/unknownrori",
+    src: LinkedinIcon,
+  },
+  {
+    alt: "Proton Mail",
+    href: "mailto:UnknownRori@proton.me",
+    src: ProtonMailIcon,
+  },
   { alt: "Blogspot", href: "#", src: BlogspotIcon },
-  { alt: "Steam", href: "https://steamcommunity.com/id/UnknownRori/", src: SteamIcon },
+  {
+    alt: "Steam",
+    href: "https://steamcommunity.com/id/UnknownRori/",
+    src: SteamIcon,
+  },
   { alt: "Figma", href: "https://www.figma.com/@UnknownRori", src: FigmaIcon },
-  { alt: "Freecodecamp", href: "https://www.freecodecamp.org/UnknownRori", src: FreecodecampIcon },
-  { alt: "crates.io", href: "https://crates.io/users/UnknownRori", src: CratesIOIcon },
-  { alt: "packagist", href: "https://packagist.org/packages/unknownrori/", src: PackagistIcon },
+  {
+    alt: "Freecodecamp",
+    href: "https://www.freecodecamp.org/UnknownRori",
+    src: FreecodecampIcon,
+  },
+  {
+    alt: "crates.io",
+    href: "https://crates.io/users/UnknownRori",
+    src: CratesIOIcon,
+  },
+  {
+    alt: "packagist",
+    href: "https://packagist.org/packages/unknownrori/",
+    src: PackagistIcon,
+  },
 ];
 
 export default socialMediaLinks;
