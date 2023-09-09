121ab3f21e3f606478f5c1a047adf0ecf0b466bf
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-09-10 05:44:05 +0700

    🛠️ Fix background

--- layouts/DefaultLayout.vue
+++ layouts/DefaultLayout.vue
@@ -5,7 +5,7 @@
     class="fixed top-0 left-0 w-full h-full bg-black bg-[url(~/assets/image/background.jpg)] pointer-events-none -z-10"
   ></div>
   <div
-    class="flex flex-col w-full min-h-screen m-0 p-0 bg-black text-white bg-[url(~/assets/icon/Grid.svg)]"
+    class="flex flex-col w-full min-h-screen m-0 p-0 bg-black text-white bg-[url(~/assets/icon/Grid.svg)] bg-opacity-80"
   >
     <NavigationBarVue />
     <div class="h-8 overflow-hidden mx-10">
