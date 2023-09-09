dd3aed844b41cbb17317d8ecb79ff07989da2ede
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-09-09 13:07:58 +0700

    📦 Improve 404

--- error.vue
+++ error.vue
@@ -10,12 +10,8 @@ onMounted(() => {
 </script>
 
 <template>
-  <NuxtLayout name="404">
-    <main
-      class="flex flex-col justify-center items-center gap-4 min-h-screen min-w-screen"
-    >
-      <h1 class="text-6xl">404</h1>
-      <h2 class="text-4xl">Page not found!</h2>
-    </main>
+  <NuxtLayout name="default-layout">
+    <NuxtLayout name="404">
+    </NuxtLayout>
   </NuxtLayout>
 </template>
