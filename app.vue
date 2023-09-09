b876bfb067bc685152188bef0fbfa4d5d723903e
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-09-10 03:18:38 +0700

    📦 Extract navigation and loading indicator and fix flash

--- app.vue
+++ app.vue
@@ -38,14 +38,7 @@ const isDummyLoadingDone = ref(false);
   </PopoutVue>
 
   <Transition>
-    <NuxtLayout v-if="isDummyLoadingDone" name="default-layout">
-      <NavigationBarVue />
-      <div class="h-8 overflow-hidden mx-10">
-        <div
-          class="bg-[url(~/assets/icon/Seperator.svg)] bg-repeat-x py-4"
-        ></div>
-      </div>
-      <NuxtLoadingIndicator />
+    <NuxtLayout name="default-layout">
 
       <main>
         <NuxtPage />
--- layouts/DefaultLayout.vue
+++ layouts/DefaultLayout.vue
@@ -1,10 +1,18 @@
 <template>
   <div
-    class="fixed top-0 left-0 w-full h-full bg-[url(~/assets/image/background.jpg)] opacity-10 pointer-events-none"
+    class="fixed top-0 left-0 w-full h-full bg-black bg-[url(~/assets/image/background.jpg)] pointer-events-none -z-10"
   ></div>
   <div
     class="flex flex-col w-full min-h-screen m-0 p-0 bg-black text-white bg-[url(~/assets/icon/Grid.svg)]"
   >
+    <NavigationBarVue />
+    <div class="h-8 overflow-hidden mx-10">
+      <div
+        class="bg-[url(~/assets/icon/Seperator.svg)] bg-repeat-x py-4"
+      ></div>
+    </div>
+    <NuxtLoadingIndicator />
+
     <slot />
   </div>
 </template>
