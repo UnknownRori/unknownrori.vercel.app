1920f9afd09a57f6f05468b144515a57ab5db792
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-09-09 10:25:28 +0700

    🚀 Added background image

--- layouts/DefaultLayout.vue
+++ layouts/DefaultLayout.vue
@@ -1,5 +1,8 @@
 <template>
   <div
+    class="fixed top-0 left-0 w-full h-full bg-[url(~/assets/image/background.jpg)] opacity-10 pointer-events-none"
+  ></div>
+  <div
     class="flex flex-col w-full min-h-screen m-0 p-0 bg-black text-white bg-[url(~/assets/icon/Grid.svg)]"
   >
     <slot />
