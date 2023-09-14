1e9bca2e8ac442f5cd15218a68d96db2e1f75942
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-09-14 15:44:42 +0700

    🛠️ Reverting from extracting default layout

--- layouts/DefaultLayout.vue
+++ layouts/DefaultLayout.vue
@@ -11,6 +11,10 @@ useBlobity();
   <div
     class="m-0 flex min-h-screen w-full flex-col bg-black bg-opacity-80 bg-[url(~/assets/icon/Grid.svg)] p-0 text-white"
   >
+    <NavigationBarVue />
+    <div class="mx-10 h-8 overflow-hidden">
+      <div class="bg-[url(~/assets/icon/Seperator.svg)] bg-repeat-x py-4"></div>
+    </div>
     <NuxtLoadingIndicator />
 
     <slot />
--- layouts/GuestLayout.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <NavigationBarVue />
-  <div class="mx-10 h-8 overflow-hidden">
-    <div class="bg-[url(~/assets/icon/Seperator.svg)] bg-repeat-x py-4"></div>
-  </div>
-
-  <slot />
-</template>
