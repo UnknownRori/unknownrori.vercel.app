e4a06d2b1105f8509e0eb33ccf8eb44c50d1592c
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-09-17 05:38:39 +0700

    🛠️ Extract Project Card to it's own component

---
+++ components/ProjectCardVue.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import Project from "~/types/project";
+
+defineProps<{
+  project: Project
+}>();
+</script>
+
+<template>
+    <CardVue
+      class="divide-2 flex w-full flex-col gap-1 rounded bg-opacity-60"
+    >
+      <header>
+        <NuxtImg
+          :src="$props.project?.img_url"
+          sizes="sm:100vw md:50vw lg:400px"
+          format="webp"
+          placeholder
+        />
+        <div class="flex items-center gap-1 p-2">
+          <h1 class="px-4 text-xl font-bold tracking-wide">
+            {{ $props.project?.title }}
+          </h1>
+          <a
+            v-if="$props.project?.source_url"
+            :href="$props.project?.source_url"
+            target="_blank"
+            class="rounded"
+          >
+            <NuxtImg src="/social-icon/github.webp" class="w-6" placeholder />
+          </a>
+          <a
+            v-if="$props.project?.deployment_url"
+            :href="$props.project?.deployment_url"
+            target="_blank"
+            class="rounded"
+          >
+            <LinkIcon class="w-6" :font-controlled="false" filled />
+          </a>
+        </div>
+      </header>
+      <main
+        class="mx-4 mb-4 flex h-full flex-col justify-between gap-2 border-t-2 border-gray-700"
+      >
+        <p>
+          {{ $props.project?.description }}
+        </p>
+
+        <ul class="flex flex-wrap gap-2">
+          <li v-for="(tag, idx) in project.tags.split(',')" :key="idx">
+            <TagVue :text="tag" />
+          </li>
+        </ul>
+      </main>
+    </CardVue>
+</template>
--- pages/projects.vue
+++ pages/projects.vue
@@ -19,53 +19,6 @@ const { data: projects } = await useFetch<ProjectResult>("/api/v1/projects", {
     v-if="projects != null"
     class="mx-12 my-8 grid flex-wrap justify-center gap-12 sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3"
   >
-    <CardVue
-      v-for="(project, idx) in projects.data"
-      :key="idx"
-      class="divide-2 flex w-full flex-col gap-1 rounded bg-opacity-60"
-    >
-      <header>
-        <NuxtImg
-          :src="project?.img_url"
-          sizes="sm:100vw md:50vw lg:400px"
-          format="webp"
-          placeholder
-        />
-        <div class="flex items-center gap-1 p-2">
-          <h1 class="px-4 text-xl font-bold tracking-wide">
-            {{ project?.title }}
-          </h1>
-          <a
-            v-if="project?.source_url"
-            :href="project?.source_url"
-            target="_blank"
-            class="rounded"
-          >
-            <NuxtImg src="/social-icon/github.webp" class="w-6" placeholder />
-          </a>
-          <a
-            v-if="project?.deployment_url"
-            :href="project?.deployment_url"
-            target="_blank"
-            class="rounded"
-          >
-            <LinkIcon class="w-6" :font-controlled="false" filled />
-          </a>
-        </div>
-      </header>
-      <main
-        class="mx-4 mb-4 flex h-full flex-col justify-between gap-2 border-t-2 border-gray-700"
-      >
-        <p>
-          {{ project?.description }}
-        </p>
-
-        <ul class="flex flex-wrap gap-2">
-          <li v-for="(tag, idx) in project.tags.split(',')" :key="idx">
-            <TagVue :text="tag" />
-          </li>
-        </ul>
-      </main>
-    </CardVue>
+    <ProjectCardVue v-for="(project, idx) in projects.data" :key="idx" :project="project" />
   </main>
 </template>
