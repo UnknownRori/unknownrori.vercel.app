387581ac118d883c5a40bba02043f01b832bd30e
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-09-17 15:51:44 +0700

    📦 Added image preview

---
+++ components/PreviewImageVue.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import CloseIcon from "~/assets/icon/Close.svg";
+
+defineProps<{
+  isOpen: boolean;
+  src: string;
+}>();
+
+defineEmits<{
+  (e: "close"): void;
+  (e: "click-outside"): void;
+}>();
+</script>
+
+<template>
+  <PopoutVue :is-open="$props.isOpen" @click-outside="$emit('click-outside')">
+    <button class="absolute right-0 top-0 m-2" @click="$emit('close')">
+      <CloseIcon class="w-8" :font-controlled="false" filled />
+    </button>
+    <NuxtImg
+      :src="$props.src"
+      class="object-cover sm:w-[95vw] md:w-[80vw] lg:w-[80vw]"
+      sizes="sm:95vw md:80vw lg:80vw"
+      format="webp"
+      placeholder
+    />
+  </PopoutVue>
+</template>
--- components/ProjectCardVue.vue
+++ components/ProjectCardVue.vue
@@ -2,55 +2,65 @@
 import Project from "~/types/project";
 
 defineProps<{
-  project: Project
+  project: Project;
 }>();
+
+const imagePreviewOpen = ref(false);
 </script>
 
 <template>
-    <CardVue
-      class="divide-2 flex w-full flex-col gap-1 rounded bg-opacity-60"
-    >
-      <header>
+  <CardVue class="divide-2 flex w-full flex-col gap-1 rounded bg-opacity-60">
+
+    <PreviewImageVue
+      :src="$props.project?.img_url"
+      :is-open="imagePreviewOpen"
+      @close="imagePreviewOpen = false"
+      @click-outside="imagePreviewOpen = false"
+    />
+
+    <header>
+      <button @click="imagePreviewOpen = true">
         <NuxtImg
           :src="$props.project?.img_url"
           sizes="sm:100vw md:50vw lg:400px"
           format="webp"
           placeholder
         />
-        <div class="flex items-center gap-1 p-2">
-          <h1 class="px-4 text-xl font-bold tracking-wide">
-            {{ $props.project?.title }}
-          </h1>
-          <a
-            v-if="$props.project?.source_url"
-            :href="$props.project?.source_url"
-            target="_blank"
-            class="rounded"
-          >
-            <NuxtImg src="/social-icon/github.webp" class="w-6" placeholder />
-          </a>
-          <a
-            v-if="$props.project?.deployment_url"
-            :href="$props.project?.deployment_url"
-            target="_blank"
-            class="rounded"
-          >
-            <LinkIcon class="w-6" :font-controlled="false" filled />
-          </a>
-        </div>
-      </header>
-      <main
-        class="mx-4 mb-4 flex h-full flex-col justify-between gap-2 border-t-2 border-gray-700"
-      >
-        <p>
-          {{ $props.project?.description }}
-        </p>
+      </button>
+      <div class="flex items-center gap-1 p-2">
+        <h1 class="px-4 text-xl font-bold tracking-wide">
+          {{ $props.project?.title }}
+        </h1>
+        <a
+          v-if="$props.project?.source_url"
+          :href="$props.project?.source_url"
+          target="_blank"
+          class="rounded"
+        >
+          <NuxtImg src="/social-icon/github.webp" class="w-6" placeholder />
+        </a>
+        <a
+          v-if="$props.project?.deployment_url"
+          :href="$props.project?.deployment_url"
+          target="_blank"
+          class="rounded"
+        >
+          <LinkIcon class="w-6" :font-controlled="false" filled />
+        </a>
+      </div>
+    </header>
+    <main
+      class="mx-4 mb-4 flex h-full flex-col justify-between gap-2 border-t-2 border-gray-700"
+    >
+      <p>
+        {{ $props.project?.description }}
+      </p>
 
-        <ul class="flex flex-wrap gap-2">
-          <li v-for="(tag, idx) in project.tags.split(',')" :key="idx">
-            <TagVue :text="tag" />
-          </li>
-        </ul>
-      </main>
-    </CardVue>
+      <ul class="flex flex-wrap gap-2">
+        <li v-for="(tag, idx) in project.tags.split(',')" :key="idx">
+          <TagVue :text="tag" />
+        </li>
+      </ul>
+    </main>
+  </CardVue>
 </template>
