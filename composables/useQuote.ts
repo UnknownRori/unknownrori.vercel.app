90c2b86a3e568a00e1d024e4a85faf6d3e75cf26
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-11-30 18:46:20 +0700

    🛠️ Fix type definition

--- composables/useQuote.ts
+++ composables/useQuote.ts
@@ -1,7 +1,7 @@
 const BASE_API_URL_QUOTES = "https://api.quotable.io";
 const RANDOM_QUOTES = `${BASE_API_URL_QUOTES}/random`;
 
-export default async function (): Quote {
+export default async function (): Promise<Quote> {
   const resultFetch = await fetch(RANDOM_QUOTES);
   const result = await resultFetch.json();
   return result;
