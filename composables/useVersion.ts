2b5fbea214056391374564ad4f4d69a04bf81f5d
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-12-08 04:41:16 +0700

    🚀 Bump main version and run lint

--- composables/useVersion.ts
+++ composables/useVersion.ts
@@ -1,4 +1,4 @@
 // TODO : Maybe extract this so it's imported everywhere, why use composable
 export default function () {
-  return "0.1.0";
+  return "0.2.0";
 }
--- layouts/DefaultLayout.vue
+++ layouts/DefaultLayout.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import type { Container } from 'tsparticles-engine';
+import type { Container } from "tsparticles-engine";
 
 const onLoad = (container: Container) => {
   // Do something with the container
@@ -13,13 +13,17 @@ useBlobity();
 <template>
   <EditorOverlayVue />
 
-
-  <NuxtParticles id="tsparticles" url="/particles.json" @load="onLoad"></NuxtParticles>
+  <NuxtParticles
+    id="tsparticles"
+    url="/particles.json"
+    @load="onLoad"
+  ></NuxtParticles>
   <div
-    class="pointer-events-none fixed left-0 top-0 -z-10 h-full w-full bg-black bg-[url(/image/background.webp)] bg-cover bg-no-repeat">
-  </div>
+    class="pointer-events-none fixed left-0 top-0 -z-10 h-full w-full bg-black bg-[url(/image/background.webp)] bg-cover bg-no-repeat"
+  ></div>
   <div
-    class="m-0 flex min-h-screen w-full flex-col bg-black bg-opacity-80 bg-[url(~/assets/icon/Grid.svg)] p-0 text-white">
+    class="m-0 flex min-h-screen w-full flex-col bg-black bg-opacity-80 bg-[url(~/assets/icon/Grid.svg)] p-0 text-white"
+  >
     <NavigationBarVue />
     <div class="mx-10 h-8 overflow-hidden">
       <div class="bg-[url(~/assets/icon/Seperator.svg)] bg-repeat-x py-4"></div>
