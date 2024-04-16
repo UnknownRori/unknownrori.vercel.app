301f10279d9f496ef20f7494a74229df61cec763
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2024-04-17 06:02:35 +0700

    📦 added some note if it's slow when open IFrame

--- components/IFrameWindowVue.vue
+++ components/IFrameWindowVue.vue
@@ -20,17 +20,16 @@ defineEmits<{
         <div class="flex gap-2">
           <ComputerIcon class="w-4" :font-controlled="false" filled />
           <span> Live Demo </span>
+          <a className="text-gray-200" :href="$props.src" target="_blank" title="Open this link if it's slow"
+            @click="$emit('close')">
+            (Open new tab)
+          </a>
         </div>
         <button class="right-0 top-0 m-2" @click="$emit('close')">
           <CloseIcon class="w-6" :font-controlled="false" filled />
         </button>
       </header>
-      <iframe
-        v-if="$props.isOpen"
-        :src="$props.src"
-        class="m-0 h-full w-full p-0"
-        loading="lazy"
-      ></iframe>
+      <iframe v-if="$props.isOpen" :src="$props.src" class="m-0 h-full w-full p-0" loading="lazy"></iframe>
     </div>
   </PopoutVue>
 </template>
