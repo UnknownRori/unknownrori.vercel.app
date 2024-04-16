9b03d59714d5528846be6f43c1eed924cc4f0dbd
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2024-04-16 16:41:26 +0700

    🛠️ make the featured show first

--- server/api/v1/projects.ts
+++ server/api/v1/projects.ts
@@ -5,7 +5,10 @@ import type { Database } from "~/types/supabase";
 export default defineEventHandler(async (event) => {
   const client = await serverSupabaseClient<Database>(event);
 
-  const result = await client.from("projects").select("*");
+  const result = await client
+    .from("projects")
+    .select("*")
+    .order("featured", { ascending: false });
 
   return {
     code: result.status,
