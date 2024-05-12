27b99aaee5bc5264352392cfb1c4c8c5b45f9fde
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2024-05-12 09:24:23 +0700

    📦 scaffold uwu mode

--- app.vue
+++ app.vue
@@ -2,20 +2,23 @@
 const isDummyLoadingDone = ref(false);
 const route = useRoute();
 const isRaw = ref("raw" in route.query && route.query.raw === "true");
+const isUwU = ref("uwu" in route.query && route.query.uwu === "true");
 </script>
 
 <template>
-  <LoadingScreenVue v-if="!isRaw" :show="!isDummyLoadingDone" @done="isDummyLoadingDone = true" />
+  <div :class="isUwU ? 'font-[Yuruka]' : ''">
+    <LoadingScreenVue v-if="!isRaw" :show="!isDummyLoadingDone" @done="isDummyLoadingDone = true" />
 
-  <Transition v-if="!isRaw">
-    <NuxtLayout name="default-layout">
-      <main v-show="isDummyLoadingDone" class="h-full">
-        <NuxtPage />
-      </main>
-    </NuxtLayout>
-  </Transition>
+    <Transition v-if="!isRaw">
+      <NuxtLayout name="default-layout">
+        <main v-show="isDummyLoadingDone" class="h-full">
+          <NuxtPage />
+        </main>
+      </NuxtLayout>
+    </Transition>
 
-  <NuxtPage v-if="isRaw" />
+    <NuxtPage v-if="isRaw" />
+  </div>
 </template>
 
 <style>
