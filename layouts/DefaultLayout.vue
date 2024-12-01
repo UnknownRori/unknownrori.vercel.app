9a391d324336c2d17969d9e5db615bffb2e48e91
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2024-12-02 05:50:47 +0700

    📦 beautiful yama for background

--- layouts/DefaultLayout.vue
+++ layouts/DefaultLayout.vue
@@ -16,10 +16,9 @@ if (process.client) {
 
   <NuxtParticles id="pageParticles" url="/particles/stars.json" @load="onLoad"></NuxtParticles>
   <div
-    class="pointer-events-none fixed left-0 top-0 -z-10 h-full w-full bg-black bg-[url(/image/background.webp)] bg-cover bg-no-repeat">
+    class="pointer-events-none fixed left-0 top-0 -z-10 h-full w-full bg-black bg-[url(/image/19-1366.gif)] bg-cover bg-no-repeat">
   </div>
-  <div
-    class="m-0 flex min-h-screen w-full flex-col bg-black bg-opacity-80 bg-[url(~/assets/icon/Grid.svg)] p-0 text-white">
+  <div class="m-0 flex min-h-screen w-full flex-col bg-black bg-opacity-80 bg-[url(/image/Grid.png)] p-0 text-white">
     <NavigationBarVue />
     <div class="mx-10 h-8 overflow-hidden">
       <div class="bg-[url(~/assets/icon/Seperator.svg)] bg-repeat-x py-4"></div>
