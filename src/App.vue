5de9155a008459cdcdd900426af12ea23658e2a8
UnknownRori <unknownrori@proton.me>
2026-04-28 05:12:48 +0700

    add: blink transition

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
 import { BaseLayout } from '@/layouts';
 import { RouterView } from 'vue-router';
+import BlinkTransition from '@/components/ui/animation/BlinkTransition.vue';
 </script>
 
 <template>
   <BaseLayout>
     <RouterView v-slot='{ Component }'>
       <KeepAlive>
-        <component :is='Component' />
+        <BlinkTransition>
+          <component :is='Component' />
+        </BlinkTransition>
       </KeepAlive>
     </RouterView>
   </BaseLayout>
---
+++ src/components/ui/animation/BlinkTransition.vue
@@ -0,0 +1,80 @@
+<template>
+  <Transition>
+    <slot />
+  </Transition>
+</template>
+
+<style scoped>
+@keyframes BlinkIn {
+  0% {
+    opacity: 0;
+    filter: blur(20px);
+    transform: scale(1.2);
+  }
+
+  20% {
+    opacity: 1;
+    filter: blur(5px);
+    transform: scale(1);
+  }
+
+  40% {
+    opacity: 0.8;
+    transform: scale(1.05);
+  }
+
+  60% {
+    opacity: 1;
+  }
+
+  100% {
+    opacity: 1;
+    transform: scale(1);
+  }
+}
+
+@keyframes BlinkOut {
+  0% {
+    opacity: 1;
+    transform: scale(1);
+  }
+
+  20% {
+    opacity: 0.8;
+    transform: scale(1.05);
+  }
+
+  40% {
+    opacity: 0.9;
+    filter: blur(5px);
+  }
+
+  60% {
+    opacity: 0;
+    transform: scale(1.2);
+    filter: blur(20px);
+  }
+
+  100% {
+    opacity: 0;
+  }
+}
+
+.v-enter-active {
+  animation: BlinkIn 0.5s ease-in-out;
+}
+
+.v-leave-active {
+  animation: BlinkOut 0.5s ease-in-out;
+}
+
+.v-enter-to,
+.v-leave-from {
+  opacity: 1;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
