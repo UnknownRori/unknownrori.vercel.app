281e384af1e1b4b2f44100be7e6267123ca91f77
UnknownRori <unknownrori@proton.me>
2026-04-28 17:22:21 +0700

    update: scrambler

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -2,34 +2,46 @@
 import { vec2 } from '@/maths/vector2';
 import { WindowBase } from '@/components/ui/window';
 import { useDevice } from '@/composable/useDevice';
+import { useScrambler } from '@/composable/useScrambler';
 
 // Why? well, I hope you like normalness
 const {isMobile}  = useDevice();
+const defaultOptsScrambler = {
+  duration: 1500,
+  interval: 69,
+  autoPlay: true,
+};
+
+const profileTxt = useScrambler('PROFILE.TXT', defaultOptsScrambler);
+const profileGif = useScrambler('PROFILE.GIF', defaultOptsScrambler);
+const realName = useScrambler('Akbar Hendra Jaya', defaultOptsScrambler);
+const alias = useScrambler('@UnknownRori', defaultOptsScrambler);
+const jobs = useScrambler('Web Dev, Game Dev, Pixel Artist, VGM Composer', defaultOptsScrambler);
 </script>
 
 <template>
   <div class="relative flex md:flex-row sm:flex-col w-screen h-screen items-center">
     <div class="w-full h-full flex justify-items-center justify-center items-center">
-      <WindowBase title="PROFILE.TXT" class="md:min-w-75 sm:h-auto md:min-h-75"
+      <WindowBase :title="profileTxt.displayText" class="md:min-w-75 sm:h-auto md:min-h-75"
         :draggable="!isMobile">
         <p class="text-3xl">
-          Akbar Hendra Jaya
+          {{ realName.displayText }}
         </p>
         <p class="text-2xl">
-          @UnknownRori
+          {{ alias.displayText }}
         </p>
         <p class="text-gray-500">
-          Web Dev, Game Dev, Pixel Artist, VGM Composer
+          {{ jobs.displayText }}
         </p>
 
       </WindowBase>
     </div>
     <div class="w-full h-full relative flex justify-items-center justify-center items-center">
-      <WindowBase title="PROFILE.PNG" class="sm:relative sm:hidden md:block md:absolute md:w-75 md:left-46"
+      <WindowBase :title="profileGif.displayText" class="sm:relative sm:hidden md:block md:absolute md:w-75 md:left-46"
         :draggable="!isMobile">
         <img src="/assets/img/old/me.gif" alt="Lilith Itou" class="w-75 h-96 object-cover object-top"/>
       </WindowBase>
-      <WindowBase title="PROFILE.PNG" class="sm:relative md:absolute md:w-75 md:left-0"
+      <WindowBase :title="profileGif.displayText" class="sm:relative md:absolute md:w-75 md:left-0"
         :draggable="!isMobile">
         <img src="/assets/img/me.gif" alt="Lilith Itou" class="w-75 object-cover object-top"/>
       </WindowBase>
